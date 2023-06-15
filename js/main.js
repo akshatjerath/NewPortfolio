ed33f118c6256a10a1fa28afd2a1e20c13f372bd
Akshat <43877609+akshatjerath@users.noreply.github.com>
2023-06-15 03:22:43 +0100

    fsffwww

--- js/main.js
+++ js/main.js
@@ -37,9 +37,9 @@ $(window).on('load', function() {
 	/* -----------------------------------
 			  2. Sound Setup
 	----------------------------------- */
-	// $('body').append('<audio loop autoplay volume="1" id="audio-player"><source src="music.mp3" type="audio/mpeg"></audio>');
+	$('body').append('<audio loop autoplay volume="1" id="audio-player"><source src="music.mp3" type="audio/mpeg"></audio>');
     	var audio = document.getElementById("audio-player");
-    	audio.volume = 0.2;
+    	audio.volume = 0.0;
 	
 	if($(window).length) {
 		$('.music-bg').css({'visibility':'visible'});
